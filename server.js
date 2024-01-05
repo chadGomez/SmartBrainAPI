e38bded9d84604643553e37c4819cfb3fcbde1b5
chadGomez <chadegomez@yahoo.com>
2024-01-04 17:54:00 -0800

    updated import syntax for handleRegister, added environmental variables, installed node-fetch

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const register = require('./controllers/register');
+// const register = require('./controllers/register');
+import handleRegister from './controllers/register';
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
@@ -13,10 +14,17 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'test',
-    database: 'SmartBrain'
+    connectionString: process.env.DATABASE_URL;
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
+    // host: '127.0.0.1',
+    // user: 'postgres',
+    // password: 'test',
+    // database: 'SmartBrain'
   }
 });
 
@@ -24,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
-app.post('/register', register.handleRegister(db, bcrypt));
+app.post('/register', handleRegister.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db) );
 app.put('/image', image.handleImage(db));
 app.post('/imageurl', (req, resp) => { image.handleApiCall(req, resp) });
