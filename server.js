2ddf7724cbfec799cf2fe4e94be994f01b0ea3ff
chadGomez <chadegomez@yahoo.com>
2024-01-05 12:26:42 -0800

    change

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const path = require('path');
 
 const register = require('./controllers/register');
 // import handleRegister from './controllers/register';
@@ -33,7 +32,6 @@ const db = knex({
 app.use(bodyParser.json());
 app.use(cors());
 // error fix
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
