f37c7406a1376163c1581dd7515628078f69a4d7
chadGomez <chadegomez@yahoo.com>
2024-01-05 13:19:29 -0800

    updated api key to env variable

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: 'a8b3cc27bc9042d99ac534dbbac8338b'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, resp) => {
